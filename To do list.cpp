a6d3e3e39994f69e6d526f277e18565258ceded1
Nikita Sychev <nikitasychev@inbox.ru>
2022-10-30 11:01:27 +0500

    Add project files.

---
+++ To do list.cpp
@@ -0,0 +1,28 @@
+//Этот проект создан с помощью классического шаблона
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int m, n;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
