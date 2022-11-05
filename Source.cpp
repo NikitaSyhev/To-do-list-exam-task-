55ed2aa273eda200d043f443aa6dfd2f1b6b0402
Nikita Sychev <nikitasychev@inbox.ru>
2022-11-05 15:58:43 +0500

    Создана мультилистинговая система

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,16 @@
 //Этот проект создан с помощью классического шаблона
+
+
+
+
+
 #include <iostream>
+#include <string>
+#include "TaskManager.h"
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int m, n;
+	
 
 
 
