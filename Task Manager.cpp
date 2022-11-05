a8edf5febb1634bd76a3fa428204e8395bbcb319
Nikita Sychev <nikitasychev@inbox.ru>
2022-11-05 17:10:20 +0500

    Набросал функции для работы с задачами menu add note. delete note

--- Task Manager.cpp
+++ Task Manager.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 #include "TaskManager.h"
+#include <fstream>
+
+int menu(int x) {
+	switch (x) {
+	case 1:
+		std::cout << "Добавьте задачу:\n";
+		return 1;
+	case 2:
+		std::cout << "Вы выбрали задачу:\n";
+		return 2;
+	case 3:
+		std::cout << "Вы очистили список:\n";
+		return 3;
+	}
+}
--- TaskManager.h
+++ TaskManager.h
@@ -1,3 +1,9 @@
 #pragma once
 #include <iostream>
 #include <string>
+
+void show_menu(); // вывод изначального меню ( может быть без нее )
+int menu(int x); //работа с меню
+void add_note();//добавить заметку
+void delete_note(); //удалить заметку
+	
